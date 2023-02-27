5cba307719b85f6df37d0fb653e04868a5cc299b
shwethab8888 <shwethab8888@gmail.com>
2023-02-27 13:12:24 +0530

    flip coin in core programs

---
+++ Core_Programming/Core_Programming/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Core_Programming
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to core programming");
+        }
+    }
+}
